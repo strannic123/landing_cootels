356b8046c52bc4ec05e0bb103ae892febb74336e
strannic123 <manager350@mail.ru>
2022-02-09 15:37:52 +0300

    added section Slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//slick-slide params
+$(document).ready(function(){
+    $('.slider__cards').slick({
+        dots: true,
+        centerPadding: 0,
+        accessibility: false,
+        adaptiveHeight: true,
+        arrows: false,
+});
+});
