7b41880363ff9ec8a30b9a426d89a9af74fb515e
strannic123 <manager350@mail.ru>
2022-02-10 11:05:22 +0300

    added js, refactoring code

--- js/main.js
+++ js/main.js
@@ -8,3 +8,23 @@ $(document).ready(function(){
         arrows: false,
 });
 });
+
+
+(function () {
+    const howCardsTitle = document.querySelectorAll('.how__card-title')
+
+    howCardsTitle.forEach(el => {
+        el.addEventListener('click', () => {
+            let parent = el.closest('.how__card')
+            if (parent.classList.contains('how__open')) {
+                parent.classList.remove('how__open')
+                let elem = parent.querySelector('.how__card-descr')
+                elem.style.display = 'none'
+            } else {
+                parent.classList.add('how__open')
+                let elem = parent.querySelector('.how__card-descr')
+                elem.style.display = 'block'
+            }
+        })
+    })
+})()
